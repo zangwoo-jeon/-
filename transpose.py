26d0631d88b4d03a08eee2aaa4ca0c90c187bad0
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2020-07-16 15:16:15 +0900

    Add files via upload

---
+++ transpose.py
@@ -0,0 +1,21 @@
+def transpose(mat):
+    row_idx = list(range(len(mat)))
+    col_idx = len(mat[0])
+    trans_mat = []
+    for r in row_idx:
+        result = [mat[c][r] for c in range(col_idx)]
+        trans_mat.append(result)
+    print("이 행렬의 전치 행렬은 \n")
+    return trans_mat
+    
+n = int(input("행의 갯수를 입력하세요 : \n"))
+A = []
+B = []
+
+for i in range(n):
+    number1 = list(map(int, input("첫번째 행렬의 {}번째 행의 원소를 입력하세요 : \n".format(i+1)).split()))
+    A.append(number1)
+
+    
+from pprint import pprint
+pprint(transpose(A))
