cbc0241b7d581f14308ce6ec3435456985cb76ac
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2020-07-16 15:36:58 +0900

    Create 전치행렬과의 곱셈.py

---
+++ 전치행렬과의 곱셈.py
@@ -0,0 +1,47 @@
+def mul(mat1, mat2):
+    row_idx1 = list(range(len(mat1)))
+    row_idx2 = list(range(len(mat2)))
+    col_idx1 = len(mat1[0])
+    col_idx2 = len(mat2[0])
+    row2_length = len(mat2)
+    res_mat = []
+    
+    try:
+        if (row2_length != col_idx1):
+                raise ValueError("첫번째 행렬의 열과 두번째 행렬의 행의 수가 같지 않습니다.")
+    except ValueError:
+        a = print("첫번째 행렬의 열과 두번째 행렬의 행의 수를 맞춰주세요.")
+        return a
+            
+        
+    for r in row_idx1:
+        mid_mat = []
+        for c2 in range(col_idx2):
+            each_element = 0
+            for c1 in range(col_idx1):
+                each_element += mat1[r][c1]*mat2[c1][c2]
+            mid_mat.append(each_element)  
+        res_mat.append(mid_mat) 
+    return res_mat
+    
+def transpose(mat):
+    row_idx = list(range(len(mat)))
+    col_idx = len(mat[0])
+    trans_mat = []
+    for c in range(col_idx):
+        result = [mat[r][c] for r in row_idx]
+        trans_mat.append(result)
+    print("이 행렬의 전치 행렬은 \n")
+    return trans_mat
+    
+n = int(input("행의 갯수를 입력하세요 : \n"))
+A = []
+B = []
+
+for i in range(n):
+    number1 = list(map(int, input("첫번째 행렬의 {}번째 행의 원소를 입력하세요 : \n".format(i+1)).split()))
+    A.append(number1)
+
+    
+from pprint import pprint
+pprint(mul(A, transpose(A)))
