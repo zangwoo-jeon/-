ef2ed044212acbd6ce12659e384569a7726cbcd5
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2020-07-27 20:33:10 +0900

    Create 3x3 행렬식.py

---
+++ 3x3 행렬식.py
@@ -0,0 +1,24 @@
+def det(mat):
+    row_idx = list(range(len(mat)))
+    col_idx = len(mat[0])
+    result = 0
+    for c in range(col_idx):
+        one_c = c + 1
+        two_c = one_c + 1
+        if one_c > 2:
+            one_c -= 3
+        if two_c > 2:
+            two_c -= 3
+        det = mat[0][c] * ((mat[1][one_c]*mat[2][two_c]) - (mat[1][two_c]*mat[2][one_c]))
+        result += det
+    print("이 행렬의  행렬식은 \n")
+    return result
+    
+A = []
+
+for i in range(3):
+    row_number = list(map(int, input("행렬의 {}번째 행의 원소를 입력하세요 : \n".format(i+1)).split()))
+    A.append(row_number)
+    
+from pprint import pprint
+pprint(det(A))
